7eeadca65e54f73b768407f9fe0c7c4c9fbcdfe7
Michal Zlamaniec <mcmajk3l@gmail.com>
2013-12-25 22:07:48 +0100

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,5 @@
 class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
-    flash.now[:notice] = "Welcome!"
-    flash.now[:alert] = "My birthday is in #{@owner.countdown} days"
   end
 end
